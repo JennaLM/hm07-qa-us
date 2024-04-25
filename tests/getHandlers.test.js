e2df4666a575a9cd21f253cf956742c377c63119
JennaLM <166644089+JennaLM@users.noreply.github.com>
2024-04-25 14:42:39 -0400

    Update getHandlers.test.js

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -4,7 +4,7 @@ const config = require('../config');
 test('Should return 200 status code…', async () => {
 	let actualStatusCode;
 	try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits`);
+		const response = await fetch(`${config.API_URL}/api/v1/kits?cardId=1`);
 	actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
