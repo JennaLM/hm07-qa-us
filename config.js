ee6c3437c43d2433903e29f7464125f6b6a1e0f6
JennaLM <166644089+JennaLM@users.noreply.github.com>
2024-04-25 12:55:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://cnt-bd47bdfb-def4-47d4-853e-c450d864426e.containerhub.tripleten-services.com"
 }
