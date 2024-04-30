e2a443d1ce6d6dc9b8c1220b711cd4659646b60a
Jenna Mencome <jennamencome@yahoo.com>
2024-04-30 14:13:58 -0400

    my sixth commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-952c5c8a-5590-426b-adfb-06b578e0ddf7.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-e633612d-9d33-46ba-8c0c-99526c9e7000.containerhub.tripleten-services.com"
 
 }
--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,12 +1,28 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test("Response code should be 200" , async () => {
+let actualStatusCode;
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
 			method: 'DELETE',
 		});
+	actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatusCode).toBe(200)
 });
+
+test('Response code should be ok' , async () => {
+	let responseOk;
+		try {
+			const response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
+				method: 'DELETE',
+			});
+		 responseOk = response.ok;
+		} catch (error) {
+			console.error(error);
+		}
+		expect(responseOk).toBe(true)
+	});
